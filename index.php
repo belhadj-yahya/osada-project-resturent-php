9b44b3cd40b16832eeb1eaad6fc0730251dfad64
belhadj-yahya <bitiljusgamer@gmail.com>
2025-02-13 13:49:57 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <?php
 include("db_contect.php");
-$result = useQuery($con, "SELECT  categories.category_id, categories.category_name, COUNT(dishes.category_id) AS dishes FROM categories LEFT JOIN dishes ON categories.category_id = dishes.category_id GROUP BY  categories.category_id, categories.category_name ORDER BY dishes DESC", true);
+
+$mostUseQuery = "SELECT  categories.category_id, categories.category_name, COUNT(dishes.category_id) AS dishes FROM categories LEFT JOIN dishes ON categories.category_id = dishes.category_id GROUP BY  categories.category_id, categories.category_name ORDER BY dishes DESC";
+$result = useQuery($con, $mostUseQuery , true);
 ?>
 
 <!DOCTYPE html>
@@ -23,13 +25,16 @@ $result = useQuery($con, "SELECT  categories.category_id, categories.category_na
         </nav>
     </aside>
     <div class="content">
-        <div id="dash"></div>
         <div id="categories">
-            <form method="POST">
-                <input type="text" name="categorey" id="">
-                <input type="submit" value="search" name="findcat">
-                <input type="text" name="add" id="">
-                <input type="submit" value="add Category" name="addCategory">
+            <form method="POST" class="firstForm">
+                <div class="form1">
+                    <input type="text" name="categorey" id="">
+                    <input type="submit" value="search" name="findcat">
+                </div>
+                <div class="form2">
+                    <input type="text" name="add" id="">
+                    <input type="submit" value="add Category" name="addCategory">
+                </div>
             </form>
             <div class="category">
                 <?php
@@ -43,57 +48,64 @@ $result = useQuery($con, "SELECT  categories.category_id, categories.category_na
                         $con->exec($query);
                     }
                 }
+
                 function showCategorys($result, $niddel)
                 {
                     foreach ($result as $catigory) {
                         $id = $catigory["category_id"];
                         if (str_contains(trim(strtolower($catigory["category_name"])), trim(strtolower($niddel)))) {
-                            echo "<div class='item'>";
-                            echo  "<p>" . "category: " . $catigory["category_name"] . "</p>";
-                            echo  "<p>" . "Dishes: " . $catigory["dishes"] . "</p>";
-                            echo "<div class='settings'>";
-                            echo "<button class='remove'>DELETE</button>";
-                            echo "<button class='changeTo'>modify</button>";
-                            echo "</div>";
-                            echo "</div>";
-                            echo "<dialog class='deleteDailog'>";
-                            echo "<p>are you sure you want to delete </p>";
-                            echo "<form method='POST'>";
-                            echo "<input type='hidden' name='did' value='$id'>";
-                            echo "<input type='submit' name='delete' value='sure'>";
-                            echo "</form>";
-                            echo "<button class='close'>stop</button>";
-                            echo "</dialog>";
-                            echo "<dialog class='updated'>";
-                            echo "<form method='POST'>";
-                            echo "<input type='hidden' name='id' value='$id'>";
-                            echo "<input type='text' name='newName'>";
-                            echo "<input type='submit' name='update' value='update'>";
-                            echo "</form>";
-                            echo "</dialog>";
+                            $output = <<<HTML
+                            <div class='item'>
+                                <p>category: {$catigory["category_name"]}</p>
+                                <p>Dishes: {$catigory["dishes"]}</p>
+                                <div class='settings'>
+                                    <button class='remove'>DELETE</button>
+                                    <button class='changeTo'>MODIFY</button>
+                                </div>
+                            </div>
+                            <dialog class='deleteDailog'>
+                                <h2>Are you sure you want to delete?</h2>
+                                <form class='re' method='POST'>
+                                    <input type='hidden' name='did' value='$id'>
+                                    <input type='submit' name='delete' value='YES'>
+                                </form>
+                                <button class='close'>NO</button>
+                            </dialog>
+                            <dialog class='updated'>
+                                <div class='dailogCont'>
+                                    <h2>Update category</h2>
+                                    <form method='POST'>
+                                        <input type='hidden' name='id' value='$id'>
+                                        <input type='text' name='newName'>
+                                        <input type='submit' name='update' value='Update'>
+                                    </form>
+                                    <button class='no'>Back</button>
+                                </div>
+                            </dialog>
+                            HTML;
+                            echo $output; 
                         }
                     }
                 }
+
                 if ($_SERVER["REQUEST_METHOD"] == "POST") {
                     if (isset($_POST["findcat"])) {
                         showCategorys($result, $_POST["categorey"]);
                     } elseif (isset($_POST["delete"])) {
-                        echo "we are here baby" . "<br>";
                         $id = $_POST["did"];
                         useQuery($con, "DELETE FROM categories where category_id = $id;", false);
-                        $result = useQuery($con, "SELECT  categories.category_id, categories.category_name, COUNT(dishes.category_id) AS dishes FROM categories LEFT JOIN dishes ON categories.category_id = dishes.category_id GROUP BY categories.category_id, categories.category_name ", true);
+                        $result = useQuery($con, $mostUseQuery, true);
                         showCategorys($result, "");
                     } elseif (isset($_POST["addCategory"])) {
                         $valueToAdd = $_POST["add"];
                         useQuery($con, "INSERT INTO categories(category_name) VALUES ('$valueToAdd')", false);
-                        $res  = useQuery($con, "SELECT  categories.category_id, categories.category_name, COUNT(dishes.category_id) AS dishes FROM categories LEFT JOIN dishes ON categories.category_id = dishes.category_id GROUP BY categories.category_id, categories.category_name", TRUE);
+                        $res  = useQuery($con, $mostUseQuery, TRUE);
                         showCategorys($res, "");
-                        echo "before we see the array" . "<br>";
                     } elseif (isset($_POST["update"])) {
                         $newName = $_POST["newName"];
                         $id = $_POST["id"];
                         useQuery($con, "UPDATE categories SET category_name = '$newName' WHERE category_id = $id", false);
-                        $res = useQuery($con, "SELECT  categories.category_id, categories.category_name, COUNT(dishes.category_id) AS dishes FROM categories LEFT JOIN dishes ON categories.category_id = dishes.category_id GROUP BY categories.category_id, categories.category_name", true);
+                        $res = useQuery($con, $mostUseQuery, true);
                         showCategorys($res, "");
                     }
                 } else {
@@ -101,7 +113,6 @@ $result = useQuery($con, "SELECT  categories.category_id, categories.category_na
                 }
                 ?>
             </div>
-
         </div>
     </div>
     <script src="script.js"></script>
