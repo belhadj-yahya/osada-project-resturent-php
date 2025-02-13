982d02e8106f59e8ee923100a388847e85e52e7a
belhadj-yahya <bitiljusgamer@gmail.com>
2025-02-13 13:50:28 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,66 @@
-document.addEventListener("DOMContentLoaded", function () {
+
     let delete_btn = document.querySelectorAll(".remove");
     let close_btn = document.querySelectorAll(".close");
     let modify_btn = document.querySelectorAll(".changeTo");
-    console.log(modify_btn)
-    modify_btn.forEach(btn => {
-        btn.addEventListener("click", function() {
-            let dialog  = btn.closest(".item").nextElementSibling.nextElementSibling; 
-            console.log(dialog);
-            dialog.showModal();
-        });
-    })
-    close_btn.forEach(btn => {
-        btn.addEventListener("click",function(){
-            let dialog = btn.closest(".deleteDailog")
-            dialog.close();
-        })
-    })
-    delete_btn.forEach(btn => {
-        btn.addEventListener("click", function () {
-            // Find the closest dialog to the button
-            
-            let dialog = btn.closest(".item").nextElementSibling; 
-            console.log(dialog);
-            dialog.showModal(); 
+    let no_modify_btn = document.querySelectorAll(".no");
+    if(document.querySelector(".item")){
 
+        no_modify_btn.forEach(btn => {
+            btn.addEventListener("click", function(){
+                let dialog = btn.closest(".updated");
+                dialog.close();
+            })
+        })
+        modify_btn.forEach(btn => {
+            btn.addEventListener("click", function() {
+                let dialog  = btn.closest(".item").nextElementSibling.nextElementSibling; 
+                dialog.showModal();
+            });
+        })
+        close_btn.forEach(btn => {
+            btn.addEventListener("click",function(){
+                let dialog = btn.closest(".deleteDailog")
+                dialog.close();
+            })
+        })
+        delete_btn.forEach(btn => {
+            btn.addEventListener("click", function () { 
+                let dialog = btn.closest(".item").nextElementSibling; 
+                dialog.showModal(); 
+    
+            });
         });
-    });
-});
+    }
+    if(document.querySelector(".dishes")){
+        no_modify_btn.forEach(btn => {
+            btn.addEventListener("click", function(){
+                let dialog = btn.closest(".updated");
+                console.log(dialog);
+                dialog.close();
+            })
+        })
+        modify_btn.forEach(btn => {
+            btn.addEventListener("click", function() {
+                let dialog  = btn.closest(".dishes").nextElementSibling.nextElementSibling; 
+                console.log(dialog);
+                dialog.showModal();
+            });
+        })
+        close_btn.forEach(btn => {
+            btn.addEventListener("click",function(){
+                let dialog = btn.closest(".deleteDailog")
+                console.log(dialog);
+                dialog.close();
+            })
+        })
+        delete_btn.forEach(btn => {
+            btn.addEventListener("click", function () { 
+                let dialog = btn.closest(".dishes").nextElementSibling; 
+                console.log(dialog);
+                dialog.showModal(); 
+    
+            });
+        });
+    }
+
     
